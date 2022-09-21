53822b7d202921ee97f625cd501d7fd12e9eba09
DeNice <DeNice@DESKTOP-AVQMSU7>
2022-09-21 09:46:04 +0200

    Add project files.

---
+++ TEST/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Dennis", 26);
+            SayHi("John", 28);
+            SayHi("Thomas", 28);
+
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hi {0}", name + "Your age is " + age);
+        }
+    }
+}
